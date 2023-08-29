f8da779b164c80d458f9019ba849b67953455d6f
Razat Kaur <p2508373@my365.dmu.ac.uk>
2023-08-29 23:51:32 +0100

    Updated, fixed values so sequence shows correct numbers

--- src/FibonacciSeq.java
+++ src/FibonacciSeq.java
@@ -7,7 +7,7 @@ public class FibonacciSeq {
         //defined how big sequence is in variable
         int numbers = 100;
         //for loop created to increase numbers from 1 to end number
-        for (int i = 1; i <= numbers; i++) {
+        for (int i = 0; i <= numbers; i++) {
             //result is stored in 'biginteger' datatype of fibonacci sequence created below
             BigInteger result = fibonacciNum(i);
             //prints result
@@ -15,13 +15,20 @@ public class FibonacciSeq {
         }
     }
 
-    //method created for fibonacci sequence to initialise
-    public static BigInteger fibonacciNum(int num1) {
+    public static BigInteger fibonacciNum(int num) {
+        if (num == 0) {
+            return BigInteger.valueOf(0);
+        } else if (num == 1) {
+            return BigInteger.valueOf(1);
+        }
+
+        //method created for fibonacci sequence to initialise
+
         //first and second numbers defined
         BigInteger first = BigInteger.valueOf(0);
         BigInteger second = BigInteger.valueOf(1);
         //for loop created and i initialised at 2 as first and second are defined above
-        for (int i = 2; i <= num1; i++) {
+        for (int i = 2; i <= num; i++) {
             //when i = 2, the 2nd fibonacci number is calculated and update second variable
             //when i = 3, the 3rd is calculated and updated second var again until end of sequence
             BigInteger next = first.add(second);
@@ -32,5 +39,6 @@ public class FibonacciSeq {
         return second;
     }
 }
+
 
 
